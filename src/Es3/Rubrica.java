24f30eae806876c80fefa7c293b691e8a424baed
Mario Fragnito <mariofragnitoph@gmail.com>
2024-07-30 16:16:09 +0200

    Fixed Es3

---
+++ src/Es3/Rubrica.java
@@ -0,0 +1,43 @@
+package Es3;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+public class Rubrica {
+    private Map<String, String> rubrica = new HashMap<>();
+
+    public void addContact(String name, String number) throws NullPointerException {
+        this.rubrica.putIfAbsent(name.toLowerCase(), number);
+    }
+
+    public void removeContact(String name) throws NullPointerException {
+        this.rubrica.remove(name.toLowerCase());
+    }
+
+    public String getContactName(String number) throws NoContactException {
+        Set<String> contactsKeys = this.rubrica.keySet();
+        for (String key : contactsKeys) {
+            if (Objects.equals(this.rubrica.get(key), number)) {
+                return key;
+            }
+        }
+        throw new NoContactException("No contact with that number!");
+    }
+
+    public String getContactNumber(String name) throws NoContactException {
+        String number = this.rubrica.get(name.toLowerCase());
+        if (number == null) {
+            throw new NoContactException("No contact with that name!");
+        }
+        return number;
+    }
+
+    public void printAllContacts() {
+        Set<String> contactsKeys = this.rubrica.keySet();
+        for (String key : contactsKeys) {
+            System.out.println(key + " - " + this.rubrica.get(key));
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import Es3.NoContactException;
+import Es3.Rubrica;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Rubrica rubrica = new Rubrica();
+        rubrica.addContact("Aldo", "3459244024");
+        rubrica.addContact("Giovanni", "389483948");
+        rubrica.addContact("Giacomo", "123456789");
+        rubrica.addContact("Ajeje", "344938498");
+
+        rubrica.printAllContacts();
+
+        rubrica.removeContact("Ajeje");
+        System.out.println("----------------");
+        rubrica.printAllContacts();
+
+        System.out.println("----------------");
+        try {
+            System.out.println("Aldo's number is: " + rubrica.getContactNumber("Aldo"));
+        } catch (NoContactException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("----------------");
+        try {
+            System.out.println("Searched number contact is: " + rubrica.getContactName("389483948"));
+        } catch (NoContactException e) {
+            System.out.println(e.getMessage());
+        }
+
+
     }
 }
